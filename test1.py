bd2b796225007109ca53b02ad222de0e8e8e6f17
HeeChul So <thrrkthrrk@gmail.com>
2019-08-30 16:20:22 +0900

    Test python for learn

---
+++ test1.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 30 13:52:13 2019
+
+@author: heechul
+"""
+#%%
+a=1
+print(a)
+#%%
---
+++ test_answer.py
@@ -0,0 +1,11 @@
+import pandas
+
+print('hello!')
+
+
+def func(x):
+    return x + 1
+
+
+def test_answer():
+    assert func(3) == 5
